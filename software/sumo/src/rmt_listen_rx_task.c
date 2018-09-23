ec3d935f352dc45769087ad14b6e60c99a101320
Justin Ong <justin.ong.97@gmail.com>
2018-09-23 09:52:03 +0800

    Make RMT listen on 3 GPIO pins

--- software/sumo/src/rmt_listen_rx_task.c
+++ software/sumo/src/rmt_listen_rx_task.c
@@ -2,8 +2,7 @@
 
 static const char* TAG = "RMT";
 
-void rmt_listen_rx_task(void *pvParameter)
-{
+void rmt_listen_rx_task(void *pvParameter) {
     // this task listens to the 3 channels of the receiver and outputs to the global struct receiver
     int channel = RECEIVER_CH1_CHANNEL;
     rmt_init_rx();
@@ -21,18 +20,35 @@ void rmt_listen_rx_task(void *pvParameter)
     }
 }
 
-void rmt_init_rx() {
-    rmt_config_t rmt_rx;
-    rmt_rx.channel = RECEIVER_CH1_CHANNEL;
-    rmt_rx.gpio_num = RECEIVER_CH1_GPIO_NUM;
-    rmt_rx.clk_div = RMT_RX_CLK_DIV;
-    rmt_rx.mem_block_num = 1;
-    rmt_rx.rmt_mode = RMT_MODE_RX;
-    rmt_rx.rx_config.filter_en = true;
-    rmt_rx.rx_config.filter_ticks_thresh = 100;
-    rmt_rx.rx_config.idle_threshold = RMT_RX_MAX_US * RMT_TICK_PER_US;
-    rmt_config(&rmt_rx);
-    rmt_driver_install(rmt_rx.channel, 1000, 0);
+void rmt_init_rx(void) {
+    uint8_t i;
+
+    rmt_config_t rmt_channels[RECEIVER_CHANNELS_NUM] = {
+        {
+            .channel = RECEIVER_CH1_CHANNEL,
+            .gpio_num = RECEIVER_CH1_GPIO_NUM
+        },
+        {
+            .channel = RECEIVER_CH2_CHANNEL,
+            .gpio_num = RECEIVER_CH2_GPIO_NUM
+        },
+        {
+            .channel = RECEIVER_CH3_CHANNEL,
+            .gpio_num = RECEIVER_CH3_GPIO_NUM
+        }
+    };
+
+    for (i = 0; i < RECEIVER_CHANNELS_NUM; i++) {
+        rmt_channels[i].clk_div = RMT_RX_CLK_DIV;
+        rmt_channels[i].mem_block_num = 1;
+        rmt_channels[i].rmt_mode = RMT_MODE_RX;
+        rmt_channels[i].rx_config.filter_en = true;
+        rmt_channels[i].rx_config.filter_ticks_thresh = 100;
+        rmt_channels[i].rx_config.idle_threshold = RMT_RX_MAX_US * RMT_TICK_PER_US;
+
+        rmt_config(&rmt_channels[i]);
+        rmt_driver_install(rmt_channels[i].channel, RMT_RB_SIZE, 0);
+    }
 
     ESP_LOGI(TAG, "Init");
 }
--- software/sumo/src/rmt_listen_rx_task.h
+++ software/sumo/src/rmt_listen_rx_task.h
@@ -10,16 +10,27 @@
 // determines how many clock cycles one "tick" is
 // [1..255], source is generally 80MHz APB clk
 #define RMT_RX_CLK_DIV (80000000/RMT_TICK_PER_US/1000000)
+// time before receiver goes idle
 #define RMT_RX_MAX_US 3500
+// size of ring buffer
+#define RMT_RB_SIZE 1000
+
+#define RECEIVER_CHANNELS_NUM 3
 
 #define RECEIVER_CH1_CHANNEL 0
 #define RECEIVER_CH1_GPIO_NUM 12
+
+#define RECEIVER_CH2_CHANNEL 1
+#define RECEIVER_CH2_GPIO_NUM 13
+
+#define RECEIVER_CH3_CHANNEL 2
+#define RECEIVER_CH3_GPIO_NUM 14
 
 volatile struct {
     uint16_t ch1;
 } ReceiverChannels;
 
 void rmt_listen_rx_task(void *pvParameter);
-void rmt_init_rx();
+void rmt_init_rx(void);
 
 #endif
