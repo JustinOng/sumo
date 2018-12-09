2862636cb3b022d499f48ac0adf44d8dfc480a8b
Justin Ong <justin.ong.97@gmail.com>
2018-12-09 19:44:44 +0800

    Add some test code to try and fix the direction switchiing issue

--- software/sumo/src/main.c
+++ software/sumo/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
@@ -10,14 +11,25 @@
 #include "read_vl53l0x_task.h"
 #include "pcnt_speed_task.h"
 
+#define ESP_INTR_FLAG_DEFAULT 0
+
 static const char* TAG = "main";
+
+static volatile uint32_t last_pulse = 0;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    uint32_t gpio_num = (uint32_t) arg;
+    int64_t cur_time = esp_timer_get_time();
+
+    last_pulse = cur_time / 1000;
+}
 
 uint16_t scaleReceiver(uint16_t input) {
     // takes in a value from RECEIVER_CH_MIN to RECEIVER_CH_MAX centered on RECEIVER_CH_DEADZONE and returns
     // a uint16_t where the highest bit denotes direction and the lower 10 bits denote speed
     // map function taken from https://www.arduino.cc/reference/en/language/functions/math/map/
-    const uint16_t out_min = 0;
-    const uint16_t out_max = 1023;
+    const uint16_t out_min = 50;
+    const uint16_t out_max = 150;
 
     // if input out of range force it to be within
     if (input < RECEIVER_CH_MIN) {
@@ -42,8 +54,36 @@ uint16_t scaleReceiver(uint16_t input) {
 }
 
 void write_motor_task(void *pvParameter) {
+    static uint16_t cur_dir = 0;
+    uint32_t dir_last_changed = 0;
+    uint32_t millis;
+    uint16_t dir = 0;
+
     while(1) {
-        MotorControl[0] = scaleReceiver(ReceiverChannels[0]);
+        millis = esp_timer_get_time() / 1000;
+        uint16_t speed = scaleReceiver(ReceiverChannels[1]);
+        if (speed > 0) {
+            dir = (uint16_t) 0x8000 & speed;
+        }
+
+        // if direction has changed, wait for the wheel to stop spinning before
+        // allowing the direction to change (and throttle to increase)
+        if (dir != cur_dir) {
+            if ((millis - last_pulse) > 200) {
+                dir_last_changed = millis;
+                cur_dir = dir;
+
+                MotorControl[0] = speed;
+            } else {
+                MotorControl[0] = 0;
+            }
+        } else {
+            if ((millis - dir_last_changed) < 50) {
+                MotorControl[0] = dir;
+            } else {
+                MotorControl[0] = speed;
+            }
+        }
         vTaskDelay(1);
     }
 }
@@ -51,8 +91,12 @@ void write_motor_task(void *pvParameter) {
 void dump_task(void *pvParamter) {
     int16_t current_speed = 0;
     uint16_t target_speed = 0;
+
     while(1) {
-        pcnt_get_counter_value(PCNT_UNIT_0, &current_speed);
+        ESP_LOGI(TAG, "t: %d", last_pulse);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        
+        /*pcnt_get_counter_value(PCNT_UNIT_0, &current_speed);
         pcnt_counter_clear(PCNT_UNIT_0);
 
         target_speed = (scaleReceiver(ReceiverChannels[0]) & 0x3FF) / 128;
@@ -64,7 +108,7 @@ void dump_task(void *pvParamter) {
         MotorControl[0] |= scaleReceiver(ReceiverChannels[0]) & 0x8000;
 
         ESP_LOGI(TAG, "cur: %d target: %d power: %d", current_speed, target_speed, MotorControl[0] & 0x3FF);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);*/
     }
 }
 
@@ -77,12 +121,27 @@ void logging_task(void *pvParameter) {
 
 void app_main()
 {
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = (1ULL << 25);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    io_conf.pull_down_en = 0;
+    gpio_config(&io_conf);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+    esp_err_t ok = gpio_isr_handler_add(25, gpio_isr_handler, (void*) 25);
+
+    if (ok != ESP_OK) {
+        ESP_LOGI(TAG, "Failed to init isr: %d", ok);
+    }
+
     ESP_LOGI(TAG, "Started");
-    //xTaskCreate(&dump_task, "dump_task", 2048, NULL, 100, NULL);
+    //xTaskCreate(&dump_task, "dump_task", 2048, NULL, 5, NULL);
     xTaskCreate(&write_motor_task, "write_motor_task", 2048, NULL, 10, NULL);
-    xTaskCreate(&logging_task, "logging_task", 2048, NULL, 5, NULL);
+    //xTaskCreate(&logging_task, "logging_task", 2048, NULL, 5, NULL);
     xTaskCreate(&ledc_pwm_task, "ledc_pwm_task", 2048, NULL, 5, NULL);
     xTaskCreate(&rmt_listen_rx_task, "rmt_listen_rx_task", 2048, NULL, 5, NULL);
-    // xTaskCreate(&read_vl53l0x_task, "read_vl53l0x_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&pcnt_speed_task, "pcnt_speed_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&read_vl53l0x_task, "read_vl53l0x_task", 2048, NULL, 5, NULL);
+    //xTaskCreate(&pcnt_speed_task, "pcnt_speed_task", 2048, NULL, 5, NULL);
 }
