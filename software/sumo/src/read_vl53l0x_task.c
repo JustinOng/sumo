fc204c4110c5a701daca59e71f7cc275af605a06
JustinOng <justin.ong.97@gmail.com>
2019-01-21 21:27:28 +0800

    Detect failure to initialise VL53L0X and power cycle the sensors to try and recover

--- software/sumo/src/read_vl53l0x_task.c
+++ software/sumo/src/read_vl53l0x_task.c
@@ -30,35 +30,51 @@ void read_vl53l0x_task(void *pvParamter) {
     gpio_pad_select_gpio(POWER_CONTROL_PIN);
     gpio_set_direction(POWER_CONTROL_PIN, GPIO_MODE_OUTPUT);
 
-    gpio_set_level(POWER_CONTROL_PIN, 0);
-    vTaskDelay(100/portTICK_PERIOD_MS);
-
-    gpio_set_level(POWER_CONTROL_PIN, 1);
-    vTaskDelay(10/portTICK_PERIOD_MS);
-
     uint8_t sensor_count = 0;
+    uint8_t init_successful = 0;
 
-    TickType_t start_ticks = xTaskGetTickCount();
-    while(1) {
-        if (setAddress(&sensor_config[sensor_count], 0x10+sensor_count) == ESP_OK) {
-            ESP_LOGI(TAG, "Found sensor, readdressed to %.2x", 0x10+sensor_count);
-            sensor_count++;
-        }
-        
-        if ((xTaskGetTickCount() - start_ticks) * portTICK_PERIOD_MS > 1000) {
-            ESP_LOGI(TAG, "Identified %d VL53L0X sensors", sensor_count);
-            break;
-        }
-    }
+    for(uint8_t i = 0; i < 3; i++) {
+        gpio_set_level(POWER_CONTROL_PIN, 0);
+        vTaskDelay(500/portTICK_PERIOD_MS);
 
-    for (uint8_t i = 0; i < sensor_count; i++) {
-        if (vl53l0x_init(&sensor_config[i]) != ESP_OK) {
-            ESP_LOGI(TAG, "Failed to initialise %.2x", sensor_config[i].address);
+        gpio_set_level(POWER_CONTROL_PIN, 1);
+        vTaskDelay(10/portTICK_PERIOD_MS);
+
+        TickType_t start_ticks = xTaskGetTickCount();
+        while(1) {
+            if (setAddress(&sensor_config[sensor_count], 0x10+sensor_count) == ESP_OK) {
+                ESP_LOGI(TAG, "Found sensor, readdressed to %.2x", 0x10+sensor_count);
+                sensor_count++;
+            }
+            
+            if ((xTaskGetTickCount() - start_ticks) * portTICK_PERIOD_MS > 1000 || sensor_count >= SENSORS_NUM) {
+                ESP_LOGI(TAG, "Identified %d VL53L0X sensors", sensor_count);
+                break;
+            }
+        }
+
+        if (sensor_count < SENSORS_NUM) {
             continue;
         }
 
-        startContinuous(&sensor_config[i]);
+        esp_err_t ok;
+        for (uint8_t i = 0; i < sensor_count; i++) {
+            ok = vl53l0x_init(&sensor_config[i]);
+            if (ok != ESP_OK) {
+                ESP_LOGI(TAG, "Failed to initialise %.2x", sensor_config[i].address);
+                break;
+            }
+
+            startContinuous(&sensor_config[i]);
+        }
+
+        if (ok != ESP_OK) continue;
+
+        init_successful = 1;
+        break;
     }
+
+    if (!init_successful) vTaskDelete(NULL);
     
     uint16_t range = 0;
     while(1) {
